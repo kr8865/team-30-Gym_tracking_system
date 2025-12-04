e411d5a339c57004abab8388b87f305bec788ccc
kratika agrawal <misskratikaagrawal@gmail.com>
2025-12-04 10:22:20 +0530

    gym_system

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express= require('express');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase', {
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
+
+
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+export default app; 
